6753746e963763cfe1ade5351e6af7d3a58c8260
MarkusJ0hanss0n <sukramsc@gmail.com>
2021-12-03 16:21:14 +0100

    Solved both puzzles of day 2 2021.

--- AdventOfCode.Lib/Days/2021/_2021Day2.cs
+++ AdventOfCode.Lib/Days/2021/_2021Day2.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace AdventOfCode.Lib.Days._2021
 {
+    enum Direction {Forward, Up, Down};
+
     public class _2021Day2 : BaseDay, IDay
     {
         public _2021Day2(int year, int day, bool useExampleInput = false) : base(year, day, useExampleInput)
@@ -12,12 +14,92 @@ namespace AdventOfCode.Lib.Days._2021
 
         public string FirstPuzzle()
         {
-            throw new NotImplementedException();
+            var instructions = GetInstructions(_input);
+
+            var horizontalPosition = 0;
+            var depth = 0;
+
+            foreach (var instruction in instructions)
+            {
+                switch (instruction.Key)
+                {
+                    case Direction.Forward:
+                        horizontalPosition += instruction.Value;
+                        break;
+                    case Direction.Up:
+                        depth -= instruction.Value;
+                        break;
+                    case Direction.Down:
+                        depth += instruction.Value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return (horizontalPosition * depth).ToString();
         }
 
         public string SecondPuzzle()
         {
-            throw new NotImplementedException();
+            var instructions = GetInstructions(_input);
+
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var instruction in instructions)
+            {
+                switch (instruction.Key)
+                {
+                    case Direction.Forward:
+                        horizontalPosition += instruction.Value;
+                        depth += aim * instruction.Value;
+                        break;
+                    case Direction.Up:
+                        aim -= instruction.Value;
+                        break;
+                    case Direction.Down:
+                        aim += instruction.Value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return (horizontalPosition * depth).ToString();
+        }
+
+        private List<KeyValuePair<Direction, int>> GetInstructions(string input)
+        {
+            var lines = input.GetStrArrayBySplittingOnRows();
+            var instructions = new List<KeyValuePair<Direction, int>>();
+
+            foreach (var line in lines)
+            {
+                var lineParts = line.Split(' ');
+                var direction = GetDirectionType(lineParts[0]);
+                var amount = int.Parse(lineParts[1]);
+                instructions.Add(
+                    new KeyValuePair<Direction, int>(direction, amount));
+            }
+
+            return instructions;
+        }
+
+        private Direction GetDirectionType(string direction)
+        {
+            switch (direction.ToLower())
+            {
+                case "forward":
+                    return Direction.Forward;
+                case "up":
+                    return Direction.Up;
+                case "down":
+                    return Direction.Down;
+                default:
+                    throw new Exception($"Unknown direction '{direction}'.");
+            }
         }
     }
 }
