a3e6db60f7df0f90c544bcefaffe769bbfdf846e
MarkusJ0hanss0n <sukramsc@gmail.com>
2022-11-21 14:54:19 +0100

    Added dat component and started experiment with react and css.

--- AdventOfCode.ReactGUI/src/App.js
+++ AdventOfCode.ReactGUI/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+const Day = (props) => {
+    const { id } = props
+    return (
+        <button id="{id}" className="daysButton">{id}</button>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="left">
+                    <p className="h1">Advent of Code 2022</p>
+                    <div className='daysDiv'>
+                        <Day id="1" />
+                        <Day id="2" />
+                        <Day id="3" />
+                        <Day id="4" />
+                        <Day id="5" />
+                        <br></br>
+                        <Day id="6" />
+                        <Day id="7" />
+                        <Day id="8" />
+                        <Day id="9" />
+                        <Day id="10" />
+                    </div>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
